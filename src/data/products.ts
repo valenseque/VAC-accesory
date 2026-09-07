2dfeb381eabb9152ecfacea2b404d11edcbd498e
valenseque <truya65@gmail.com>
2026-09-07 20:55:43 -0300

    Add files via upload

--- src/data/products.ts
+++ src/data/products.ts
@@ -107,7 +107,7 @@ export const products: Product[] = [
     price: 8000,
     material: 'Acero blanco',
     category: 'Pulseras',
-    image: '<blockquote class="imgur-embed-pub" lang="en" data-id="a/W4C7pTf"  ><a href="//imgur.com/a/W4C7pTf">pulsera cerrazón azul</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>',
+    image: 'https://drive.google.com/file/d/1BbsSoHCyn-Miw8pvsnonnH0Q3hze3UY9/view?usp=sharing',
     isNew: true,
     isFeatured: true
   },
